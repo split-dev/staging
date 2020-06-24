694c961c29d8509c683ebecf42e531e75be8321d
split-dev <asamusenko@splitdev.pro>
2020-06-24 19:00:46 +0300

    add mobile

--- resources/assets/scripts/routes/mobile.js
+++ resources/assets/scripts/routes/mobile.js
@@ -3,3 +3,10 @@ let widthD = document.querySelector('body').getBoundingClientRect().width;
 if(widthD < 1023) {
   $('.animate-img').addClass('start-zoom')
 }
+
+//open-menu
+$('.menu-icon').click( function (e) {
+  e.preventDefault();
+  $('.mobile-menu').addClass('open');
+  $('body').addClass('blocked');
+});
